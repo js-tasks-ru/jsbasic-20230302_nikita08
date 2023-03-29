be239edbb11f75f7bb65262d9a7b24267af40d82
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-03-29 22:32:51 +0300

    Update index.js

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+   let clickEl = document.querySelector(".toggle-text-button");
+    let text = document.querySelector('#text');
+ 
+    clickEL.addEventListener("click", handler);
+ 
+    function handler() {
+        text.hidden = !text.hidden;
+    }
 }
