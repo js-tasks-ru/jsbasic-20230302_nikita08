3219238b44d50a4a841da6a1e9e5b05b09ee92df
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 22:40:19 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,13 +1,10 @@
 function factorial(n) {
-  // ваш код...function factorial(n){
-    let result = 1;
-    while(n){
-        result *= n--;
-    }
-    return result;
-}
-factorial(0);
-factorial(1);
-factorial(3);
-factorial(5);
+  let res = n;
+	if (n === 0){
+		res = 1;
+	}
+	for (let i = 1; i < n; i++) {
+		res *= n - i;
+	}
+	return res;
 }
