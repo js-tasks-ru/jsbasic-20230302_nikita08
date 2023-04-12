10a2d2539468098fea8a1719155ddc15df2d20be
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 23:07:48 +0300

    Update index.js

--- 2-модуль/3-задание/index.js
+++ 2-модуль/3-задание/index.js
@@ -1,20 +1,16 @@
 let calculator = {
-  // let calculator = {
-  read() {
-    this.a = +prompt('a',0);
-    this.b = +prompt('b',0);
-    },
-  sum() {
-    return this.a + this.b;
-  },
-  mul() {
-    return this.a * this.b; 
-  }
+	read (a, b) {
+		this.a = a;
+		this.b = b;
+	},
+	sum () {
+		return this.a + this.b;  
+	},
+	mul () {
+		return this.a * this.b; 
+	},
 };
-calculator.read(3, 5);
-console.log(calculator.sum());
-console.log(calculator.mul());
-};
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
