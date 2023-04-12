81957a59f31648dc5044f172283e4aed6390911b
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 23:27:23 +0300

    Update index.js

--- 5-модуль/3-задание/index.js
+++ 5-модуль/3-задание/index.js
@@ -1,35 +1,38 @@
 function initCarousel() {
   let carouselArrowRight = document.querySelector('.carousel__arrow_right');
-  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+	let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
   let carouselInner = document.querySelector('.carousel__inner');
   let slides = carouselInner.children;
 
   let step = 0;
   let slideWidth = carouselInner.firstElementChild.offsetWidth;
-
+  
   if (slides[step] == carouselInner.firstElementChild) {
     carouselArrowLeft.style.display = 'none';
   }
-
+  
   function translateSlide() {
-    carouselInner.style.transform = 'translateX(' + (-slideWidth) * step + 'px)';
+    carouselInner.style.transform = `translateX(${-slideWidth * step}px)`;
+
     if (slides[step] == carouselInner.lastElementChild) {
       carouselArrowRight.style.display = 'none';
     } else {
       carouselArrowRight.style.display = '';
     }
+
     if (slides[step] == carouselInner.firstElementChild) {
       carouselArrowLeft.style.display = 'none';
     } else {
       carouselArrowLeft.style.display = '';
     }
   }
-  carouselArrowRight.addEventListener('click', function rightStep () {
+
+  carouselArrowRight.addEventListener('click', function () {
     step++;
     translateSlide();
   });
-
-  carouselArrowLeft.addEventListener('click', function lestStep () {
+  
+  carouselArrowLeft.addEventListener('click', function () {
     step--;
     translateSlide();
 	});
