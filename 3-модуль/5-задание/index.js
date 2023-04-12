5eba649e2338908bc9cceac076b236c42b7e147a
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 23:18:19 +0300

    Update index.js

--- 3-модуль/5-задание/index.js
+++ 3-модуль/5-задание/index.js
@@ -1,34 +1,12 @@
 function getMinMax(str) {
-  const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
-function getMinMax(str) {
-  let inputData =str.split(',');
-  console.log(inputData);
-  
-  let inputData2 = inputData.map((el) => el.split(' '));
-  
-  let nums = [];
-  
-  inputData2.forEach((element) => {
-    element.forEach((innerElement) => {
-      if (innerElement==='') {
-        return;
-      }
-      let num = parseFloat(innerElement,10);
-      
-      if(isNaN(num)) {
-        return;
-      }
-      
-      nums.push(num);
-    })
-  });
-  
-  const min = Math.min(...nums);
-  const max = Math.max(...nums);
-  return{
-    min,max
-  }
-}
+	let newStr = str.split(/\s*,| \s*/);
+    
+	newStr.sort( (a, b) => a - b );
+	
+	let result = {
+      min: +newStr[0],
+      max: +newStr[newStr.length - 1],
+	};
 
-console.log(getMinMax(inputData));// ваш код...
+	return result;
 }
