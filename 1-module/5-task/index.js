e75c9b79ec79c87f353d18be54ba16399c275d06
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 22:48:11 +0300

    Update index.js

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,6 +1,7 @@
 function truncate(str, maxlength) {
-  // ваш код...function truncate(str, maxlength) {
-  return (str.length > maxlength) ?
-    str.slice(0, maxlength - 1) + '…' : str;
-}
+	if (str.length > maxlength) {
+		return str.slice(0, maxlength-1) + "…";
+	} else {
+		return str;
+	}
 }
