137859e9645b761754a3e64921ad3d09f09cb4e8
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 23:14:38 +0300

    Update index.js

--- 3-модуль/2-задание/index.js
+++ 3-модуль/2-задание/index.js
@@ -1,10 +1,4 @@
 function filterRange(arr, a, b) {
-  // ваш код...function filterRange(arr, a, b) {
-  return arr.filter(item => (a <= item && item <= b));
-}
-
-let arr = [5, 3, 8, 1];
-let filtered = filterRange(arr, 1, 4);
-console.log(filtered);
-console.log(arr)
+  let filterArr = arr.filter(item => (a <= item && item <= b) );
+	return filterArr;
 }
