0965b22c940668591997093fba6c70efcbd860de
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 23:24:24 +0300

    Update index.js

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,11 +1,10 @@
 function toggleText() {
-  // ваш код...
-   let clickEl = document.querySelector(".toggle-text-button");
-    let text = document.querySelector('#text');
- 
-    clickEL.addEventListener("click", handler);
- 
-    function handler() {
-        text.hidden = !text.hidden;
-    }
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+  
+  function toggleHidden() {
+		(text.hidden == false) ? text.hidden = true : text.hidden = false;
+	}
+
+	button.addEventListener('click', toggleHidden);
 }
