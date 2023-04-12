1fc442ad85ebdc4e3a6795b13ea3327af460999a
nikita-berdnikov <126612529+nikita-berdnikov@users.noreply.github.com>
2023-04-12 23:20:23 +0300

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,11 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
   let ul = document.createElement('ul');
-  for (let item of friends) {
-      let li = document.createElement('li');
-      li.innerHTML = `${item.firstName} ${item.lastName}`;
-      ul.appendChild(li);
-  }
-  return ul;
+	for (let friend of friends) {
+		let li = document.createElement('li');
+		li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+		ul.append(li);
+	}
+	return ul;
 }
-
